0c7ccac6023edadc09f843b17f4615d4c044fa06
kuzhanthaivel <kuzhanthaivel272@gmail.com>
2024-07-20 11:34:22 +0000

    my new nada program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -4,9 +4,22 @@ def nada_main():
     party1 = Party(name="Party1")
     party2 = Party(name="Party2")
     party3 = Party(name="Party3")
+    
     a = SecretInteger(Input(name="A", party=party1))
     b = SecretInteger(Input(name="B", party=party2))
 
     result = a + b
 
-    return [Output(result, "my_output", party3)]
+    return [Output(result, "my_output", party=party3)]
+
+def simulate_protocol():
+    input_a = 5
+    input_b = 3
+
+    protocol_output = nada_main()
+
+    for output in protocol_output:
+        if output.name == "my_output":
+            print(f"Output for {output.party.name}: {output.value}")
+
+simulate_protocol()
